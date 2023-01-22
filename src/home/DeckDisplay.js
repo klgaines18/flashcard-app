ea238be3ae801e5f9332f0c39432d4d7f892ea0a
Katie Gaines <klgaines18@gmail.com>
2023-01-22 08:50:09 -0500

    updated DeckDisplay

--- src/home/DeckDisplay.js
+++ src/home/DeckDisplay.js
@@ -1,15 +1,20 @@
 import React from "react";
-import { useHistory, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const DeckDisplay = ({ deck = { cards: [] } }) => (
-  <article className="col-12 col-md-6 col-xl-3 my-2 align-self-stretch">
-    <div className="border p-4 h-100 d-flex flex-column">
-      <h2 className="font-weight-lighter flex-fill">
-        <Link to={`/decks/${deck.id}`}>{deck.name}</Link>
+  <article className="card">
+    <div className="card-body ">
+      <h2 className="card-title">
+        {deck.name}
       </h2>
-      <Link to={`/decks/${deck.id}/cards`} className="mt-2">Cards »</Link>
+      <h6 class="card-subtitle mb-2 text-muted">Card amount</h6>
+      <p className="card-text">{deck.description}</p>
+      <a href="#" class="card-link">View</a>
+      <a href="#" class="card-link">Study</a>
+      <a href="#" class="card-link">Delete</a>
     </div>
   </article>
 );
 
 export default DeckDisplay;
+
--- src/home/DeckList.js
+++ src/home/DeckList.js
@@ -24,7 +24,7 @@ export const DeckList = () => {
 
   return (
     <main className="container">
-      <section className="row">{list}</section>
+      <section className="col">{list}</section>
     </main>
   );
 };
